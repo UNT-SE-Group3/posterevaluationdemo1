2905a084fcf30e5f54a9041f209e4153ab252ae6
username <email>
2023-10-22 22:43:55 -0500

    NFR-BUG1-Prathima-Fix

--- poster-final.py
+++ poster-final.py
@@ -13,13 +13,34 @@ import json
 # Constants and Configurations
 CONFIDENCE_THRESHOLD = 60
 
+# Function to load an image from a file
 def load_image(image_path):
+    """
+    Load an image from a file.
+
+    Args:
+        image_path (str): The path to the image file.
+
+    Returns:
+        numpy.ndarray: The loaded image as a NumPy array.
+    Raises:
+        Exception: If there is an error loading the image.
+    """
     image = cv2.imread(image_path)
     if image is None:
         raise Exception("Error loading image")
     return image
 
 def extract_text(image):
+    """
+    Extract text from an image using pytesseract.
+
+    Args:
+        image (numpy.ndarray): The input image as a NumPy array.
+
+    Returns:
+        str: The extracted text in lowercase for easier comparison.
+    """
     # Extract text from the image using pytesseract
     text = pytesseract.image_to_string(image, output_type=Output.STRING)
     return text.lower()  # Convert text to lowercase for easier comparison
@@ -27,6 +48,16 @@ def extract_text(image):
 def assess_common_words(text, common_words):
     
     # Count the number of common words present in the text without duplicates
+    """
+    Count the number of common words present in the text without duplicates.
+
+    Args:
+        text (str): The text to assess.
+        common_words (list): List of common words to check for in the text.
+
+    Returns:
+        int: A score based on the number of common words found.
+    """
     unique_common_words = set(common_words)
     word_count = len([word for word in text.split() if word in unique_common_words])
     score = word_count * 3
@@ -34,6 +65,15 @@ def assess_common_words(text, common_words):
 
 def detect_title(image):
     # Detect if there is a title in the top middle of the image
+    """
+    Detect if there is a title in the top middle of the image.
+
+    Args:
+        image (numpy.ndarray): The input image as a NumPy array.
+
+    Returns:
+        int: A title detection score.
+    """
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     _, binary = cv2.threshold(gray, 128, 255, cv2.THRESH_BINARY)
     contours, _ = cv2.findContours(binary, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
@@ -54,6 +94,15 @@ def detect_title(image):
 def detect_images_and_graphs(image):
     # Detect if there are any non-text areas that may contain images or graphs
     # For simplicity, this code detects non-white regions as potential images or graphs
+    """
+    Detect if there are any non-text areas that may contain images or graphs.
+
+    Args:
+        image (numpy.ndarray): The input image as a NumPy array.
+
+    Returns:
+        int: A score indicating the presence of images or graphs.
+    """
     non_text_area = cv2.subtract(255, cv2.cvtColor(image, cv2.COLOR_BGR2GRAY))
     _, non_text_area = cv2.threshold(non_text_area, 128, 255, cv2.THRESH_BINARY)
     non_text_pixels = cv2.countNonZero(non_text_area)
@@ -64,6 +113,15 @@ def detect_images_and_graphs(image):
         return 0
 
 def detect_logo(image):
+    """
+    Detect if there is a logo in the top or top-right corner of the image.
+
+    Args:
+        image (numpy.ndarray): The input image as a NumPy array.
+
+    Returns:
+        int: A logo detection score.
+    """
     # Detect if there is a logo in the top or top-right corner of the image
     logo_region = image[:100, -100:]
     gray_logo = cv2.cvtColor(logo_region, cv2.COLOR_BGR2GRAY)
@@ -75,6 +133,16 @@ def detect_logo(image):
     else:
         return 0
 def predict(image, common_words):
+    """
+    Predict assessment scores based on image analysis.
+
+    Args:
+        image (numpy.ndarray): The input image as a NumPy array.
+        common_words (list): List of common words to check for in the text.
+
+    Returns:
+        dict: A dictionary containing assessment scores.
+    """
     # Process the image directly without reading from file
     # Since we're using 'numpy' type for the Gradio input, the 'image' is now a numpy array
     img_np = image
